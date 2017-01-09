08acba067e920cf3f164b4d6abc40afb894ba080
Simon Sun <blackghost0118@gmail.com>
2017-01-10 02:48:01 +0800

    ru;

---
+++ api/controllers/center/create.js
@@ -0,0 +1,34 @@
+
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:api:controllers:center:create');
+
+import _ from 'lodash';
+
+import { Center } from '../../../models';
+
+module.exports = async (req, res, next) => {
+
+    let options = _.pick(req.body, ['name', 'CreatedBy', 'UpdatedBy']);
+    options.Departments = JSON.parse(req.body.Departments);
+    debug('options = %j', options);
+
+    try {
+
+        let center = await Center.findOne()
+            .where('name').equals(req.body.name)
+            .where('isTrashed').equals(false)
+            .execAsync();
+        debug('center = %j', center);
+
+        if(center) {
+            throw new Error('13001');
+        }
+
+        let newCenter = await Center.createAsync(options);
+        debug('new center = %j', newCenter);
+
+        return res.json(newCenter);
+    } catch(err) {
+        return next(err);
+    }
+};
---
+++ api/controllers/department/list.js
@@ -0,0 +1,13 @@
+
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:api:controllers:department:list');
+
+import { Center } from '../../../models';
+
+module.exports = async (req, res, next) => {
+    try {
+
+    } catch(err) {
+        return next(err);
+    }
+};
