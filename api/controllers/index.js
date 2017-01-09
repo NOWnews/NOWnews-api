e01db8a4406139a2f5fdba3da6a75b65abb65c82
Simon Sun <blackghost0118@gmail.com>
2017-01-10 02:55:01 +0800

    Revert "Revert "ru;""
    This reverts commit cd0397af33a6312630b20b84fde39079bd82964e.

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -2,6 +2,8 @@ import home from './home';
 import user from './user';
 import policy from './policy';
 import role from './role';
+import center from './center';
+import department from './department';
 
 module.exports = (app) => {
 
@@ -9,6 +11,8 @@ module.exports = (app) => {
     app.use('/', user);
     app.use('/', policy);
     app.use('/', role);
+    app.use('/', center);
+    app.use('/', department);
 
     return (req, res, next) => {
         return next();
