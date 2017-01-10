46cbb6501dee31fcfa1ec1534abf72f856d92801
Simon Sun <blackghost0118@gmail.com>
2017-01-10 20:19:52 +0800

    避免被別人亂打端點

--- api/controllers/home/home.js
+++ api/controllers/home/home.js
@@ -1,19 +1,6 @@
 
 import models from '../../../models';
 
-module.exports = async(req, res, next) => {
-    // 確認端點是否正常 work
-    let foo = await models.Test.createAsync({
-        name: ' SiMon    '
-    });
-
-    let bar = await models.Test.findOne()
-        .sort('-_id')
-        .execAsync();
-
-    console.log(foo);
-    console.log(bar);
-    console.log(bar.id);
-    console.log(bar._id);
-    return res.send('<h1>Hello World!</h1>');
+module.exports = (req, res, next) => {
+    return res.send('<h1>你媽媽有說亂打端點是不道德的嗎</h1>');
 };
