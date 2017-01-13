fb20969e2b63d66befedee03626572aeca1c85ac
esbb48 <esbb48@gmail.com>
2017-01-13 17:57:01 +0800

    Add list

--- api/controllers/role/index.js
+++ api/controllers/role/index.js
@@ -5,8 +5,10 @@ let router = express.Router();
 import validators from '../../validators';
 
 import create from './create';
+import list from './list';
 
 router.route('/roles')
+    .get(list)
     .post(validators.role.create, create);
 
 module.exports = router;
