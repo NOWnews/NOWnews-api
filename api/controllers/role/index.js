7a29923d9c877c0461b4125a99474cfab82601a4
esbb48 <esbb48@gmail.com>
2017-01-13 18:39:04 +0800

    Add update

--- api/controllers/role/index.js
+++ api/controllers/role/index.js
@@ -2,19 +2,21 @@
 import express from 'express';
 let router = express.Router();
 
-import validators from '../../validators';
+import { role as validatorRole } from '../../validators';
 
 import create from './create';
 import list from './list';
 import one from './one';
 import remove from './remove';
+import update from './update';
 
 router.route('/roles')
     .get(list)
-    .post(validators.role.create, create);
+    .post(validatorRole.create, create);
 
 router.route('/roles/:id')
     .get(one)
-    .delete(remove);
+    .delete(remove)
+    .put(update);
 
 module.exports = router;
