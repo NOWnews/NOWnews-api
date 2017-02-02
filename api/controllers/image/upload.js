b58e1f693104ab4366ebb302d000beec96f9b444
Simon Sun <blackghost0118@gmail.com>
2017-02-02 12:33:02 +0800

    完成圖片上傳到圖床與回傳 url

--- api/controllers/image/upload.js
+++ api/controllers/image/upload.js
@@ -7,6 +7,8 @@ import fs from 'fs';
 import Promise from 'bluebird';
 import mongoose from 'mongoose';
 import moment from 'moment-timezone';
+import config from 'config';
+import imageServer from 'scp2';
 
 import { Image } from '../../../models';
 
@@ -41,6 +43,29 @@ module.exports = async(req, res, next) => {
         let objectId = mongoose.Types.ObjectId();
         let now = moment(Date.now()).tz('Asia/Taipei').format('YYYYMMDDHHmm');
         let newName = `${objectId}-${now}.${format.toLowerCase()}`;
+        let newPath = `uploads/${newName}`;
+
+        // 將圖片名稱換掉
+        fs.renameSync(path, newPath);
+
+        // scp 到 img.nownews.com 圖床
+        await new Promise((resolve, reject) => {
+
+            let username = config.get('imageServer.username');
+            let password = config.get('imageServer.password');
+            let host = config.get('imageServer.host');
+            let folder = config.get('imageServer.folder');
+            let port = config.get('imageServer.port');
+            let scpCommand = `${username}:${password}@${host}:${port}:${folder}`;
+
+            imageServer.scp(newPath, scpCommand, (err) => {
+                if(err) {
+                    return reject(err);
+                }
+
+                return resolve('ok');
+            });
+        });
 
         // 組成要儲存的資料
         let options = {
@@ -53,7 +78,7 @@ module.exports = async(req, res, next) => {
             mimetype,
             width: size.width,
             height: size.height,
-            url: `https://whocare.com/${newName}`,
+            url: `${config.get('imageServer.url')}/${newName}`,
             CreatedBy,
             UpdatedBy: CreatedBy
         };
@@ -65,7 +90,7 @@ module.exports = async(req, res, next) => {
 
         // 刪掉檔案
         await new Promise((resolve, reject) => {
-            fs.unlink(path, (err, result) => {
+            fs.unlink(newPath, (err, result) => {
                 if(err) {
                     return reject(err);
                 }
