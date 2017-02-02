af76537eae2400997019b12f6154c82508d2b463
Simon Sun <blackghost0118@gmail.com>
2017-02-02 10:22:01 +0800

    開設 file

---
+++ api/controllers/news/release.js
@@ -0,0 +1,15 @@
+
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:api:controllers:news:release');
+
+import _ from 'lodash';
+
+import { News } from '../../../models';
+import { newsLog } from '../../../libs';
+
+module.exports = async (req, res, next) => {
+    try {
+    }catch(err) {
+        return next(err);
+    }
+};
