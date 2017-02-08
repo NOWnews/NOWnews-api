e06010c93abc4dd987be0d628cdd3602d39dcfbd
Simon Sun <blackghost0118@gmail.com>
2017-02-08 10:47:42 +0800

    調整寫法

--- api/controllers/news/create.js
+++ api/controllers/news/create.js
@@ -33,15 +33,11 @@ module.exports = async (req, res, next) => {
             'CreatedBy'
         ]);
 
-        if(options.isAdult) {
-            let isAdult = options.isAdult === 'true' ? true : false;
-            news.set('isAdult', isAdult);
-        }
+        let isAdult = options.isAdult === true ? true : false;
+        news.set('isAdult', isAdult);
 
-        if(options.isDeliver) {
-            let isDeliver = options.isDeliver === 'true' ? true : false;
-            news.set('isDeliver', isDeliver);
-        }
+        let isDeliver = options.isDeliver === true ? true : false;
+        news.set('isDeliver', isDeliver);
 
         options.UpdatedBy = options.CreatedBy;
 
