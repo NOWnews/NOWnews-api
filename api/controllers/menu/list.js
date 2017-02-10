37193fdc01a0f43776cdc1b03cfa7ff3931efd55
Simon Sun <blackghost0118@gmail.com>
2017-02-10 13:33:24 +0800

    發現奇怪的錯誤，先暫存起來

--- api/controllers/menu/list.js
+++ api/controllers/menu/list.js
@@ -2,6 +2,10 @@
 import Debug from 'debug';
 const debug = Debug('NOWnews-api:api:controllers:menu:list');
 
+import Promise from 'bluebird';
+import _ from 'lodash';
+import is from 'is_js';
+
 import { Menu } from '../../../models';
 
 module.exports = async (req, res, next) => {
@@ -10,6 +14,60 @@ module.exports = async (req, res, next) => {
             .where('isTrashed').equals(false)
             .execAsync();
         debug('menus = %j', menus);
+
+        let menuData = [];
+
+        let [ firstLevel, secondLevel, thirdLevel ] = await Promise.all([
+            Menu.find()
+                .where('level').equals(1)
+                .sort('-weight')
+                .where('isTrashed').equals(false)
+                .execAsync(),
+            Menu.find()
+                .where('level').equals(2)
+                .sort('-weight')
+                .where('isTrashed').equals(false)
+                .lean()
+                .execAsync(),
+            Menu.find()
+                .where('level').equals(3)
+                .sort('-weight')
+                .where('isTrashed').equals(false)
+                .execAsync()
+        ]);
+        debug('First Level = %j', firstLevel);
+        debug('Second Level = %j', secondLevel);
+        debug('Third Level = %j', thirdLevel);
+
+        // 排第二層
+        let secondLevelByParant = {};
+        console.log(is.array(secondLevel));
+        _.forEach(firstLevel, (foo) => {
+            console.log(foo);
+        });
+        // _.forEach(secondLevel, (foo) => {
+        //     debug('foo = %j', foo);
+        //     // console.log(foo);
+        // });
+        // secondLevel.forEach((foo) => {
+        //     console.log(foo);
+        // });
+        // if(secondLevel && secondLevel.length !== 0) {
+        //     _.forEach(secondLevel, function(foo) {
+        //         console.log(foo);
+        //     });
+        //     // _.forEach(secondLevel, (foo, bar) => {
+        //     //     console.log(foo);
+        //     //     // let parentId = foo.ParentId + '';
+        //     //     // console.log(parentId);
+        //     //     // if(!secondLevelByParant[parentId]) {
+        //     //     //     secondLevelByParant[parentId] = [];
+        //     //     // }
+
+        //     //     // secondLevelByParant[parentId].push(menu);
+        //     // });
+        // }
+        console.log(secondLevelByParant);
 
         return res.json(menus);
     } catch (err) {
