53ec60bfdc0bce72d17dee3e3b233741fb3d663e
Simon Sun <blackghost0118@gmail.com>
2017-02-13 17:21:30 +0800

    因應前台傳入的格式不同，重構程式碼

--- api/controllers/menu/sort.js
+++ api/controllers/menu/sort.js
@@ -10,6 +10,8 @@ Wayne 產出的原始資料:
   { id: '7', parent_id: null, depth: 0, left: 10, right: 11 },
   { id: '3', parent_id: null, depth: 0, left: 12, right: 13 } ]
 
+[{"id":"58a1292375420e4cc163c62d","children":[{"id":"58a1294475420e4cc163c630"}]},{"id":"58a1292e75420e4cc163c62e"},{"id":"58a1557275420e4cc163c631"},{"id":"58a166475fb84b131f06e37c"}]
+
 Simon 產出的類似格式:
 
 [
@@ -21,6 +23,7 @@ Simon 產出的類似格式:
 import Debug from 'debug';
 const debug = Debug('NOWnews-api:api:controllers:menu:sort');
 
+import _ from 'lodash';
 import Promise from 'bluebird';
 
 import { Menu } from '../../../models';
@@ -28,40 +31,94 @@ import { Menu } from '../../../models';
 module.exports = async (req, res, next) => {
     try {
 
-        // 因為前端套件問題，要先去除掉第一個 item
-        let menus = req.body.menus.splice(1, req.body.menus.length);
+        let menus = req.body.menus;
 
-        let updateAllMenus = await Promise.map(menus, (menu) => {
-            return Menu.findById(menu.id)
-                .then((doc) => {
+        // 處理完後的資料
+        let menuData = [];
 
-                    doc.set('ParentId', menu.parent_id);
-                    doc.set('level', menu.depth);
+        // 處理第一層的資料
+        _.forEach(menus, (mainMenu, i) => {
+            menuData.push({
+                _id: mainMenu.id,
+                weight: i,
+                level: 0,
+                hasChild: mainMenu.children ? true : false,
+                ParentId: null
+            });
 
-                    // 以目前來說，第二層不會有 child
-                    if(menu.depth > 0) {
-                        doc.set('hasChild', false);
-                    }
-
-                    return doc.saveAsync();
+            // 如果第一層有 child，要處理第二層的資料
+            if(mainMenu.children) {
+                _.forEach(mainMenu.children, (child, k) => {
+                    menuData.push({
+                        _id: child.id,
+                        weight: k,
+                        level: 0,
+                        hasChild: false,
+                        ParentId: mainMenu.id
+                    });
                 });
+            }
         });
 
-        // 更新 parentId 的資料欄位
-        let updateParents = await Promise.map(menus, (menu) => {
-            if(!menu.parent_id) {
-                return Promise.resolve(null);
-            }
-
-            return Menu.findById(menu.parent_id)
+        let updateAllMenus = await Promise.map(menuData, (menu) => {
+            return Menu.findById(menu._id)
                 .then((doc) => {
-                    doc.set('hasChild', true);
+                    doc.set('weight', menu.weight);
+                    doc.set('level', menu.level);
+                    doc.set('hasChild', menu.hasChild);
+                    doc.set('ParentId', menu.ParentId);
+
                     return doc.saveAsync();
                 });
         });
 
         return res.json(updateAllMenus);
-    } catch (err) {
+    }catch (err) {
         return next(err);
     }
 };
+
+// module.exports = async (req, res, next) => {
+//     try {
+
+//         // 因為前端套件問題，要先去除掉第一個 item
+//         let menus = req.body.menus.splice(1, req.body.menus.length);
+
+//         let updateAllMenus = await Promise.map(menus, (menu) => {
+//             return Menu.findById(menu.id)
+//                 .then((doc) => {
+
+//                     doc.set('ParentId', menu.parent_id);
+//                     doc.set('level', menu.depth);
+
+//                     // 以目前來說，第二層不會有 child
+//                     let hasChild = menu.depth > 0 ? false : false;
+//                     doc.set('hasChild', hasChild);
+
+//                     return doc.saveAsync();
+//                 });
+//         });
+
+//         // 更新 parentId 的資料欄位
+//         let updateParents = await Promise.map(menus, (menu) => {
+
+//             if(!menu.parent_id) {
+//                 return Menu.findById(menu.parent_id)
+//                     .then((doc) => {
+//                         doc.set('hasChild', false);
+//                         return doc.saveAsync();
+//                     });
+//             }
+
+//             return Menu.findById(menu.parent_id)
+//                 .then((doc) => {
+//                     doc.set('hasChild', true);
+//                     return doc.saveAsync();
+//                 });
+//         });
+
+//         return res.json(updateAllMenus);
+//     } catch (err) {
+//         return next(err);
+//     }
+// };
