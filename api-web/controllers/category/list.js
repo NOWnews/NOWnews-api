b67f8973edce8439671408c08f77f14a2500a95a
Simon Sun <blackghost0118@gmail.com>
2017-03-15 17:42:56 +0800

    :star2: category news listfind news list by categoryName or type

---
+++ api-web/controllers/category/list.js
@@ -0,0 +1,68 @@
+
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:api-web:controllers:category:list');
+
+import Promise from 'bluebird';
+
+import { pagination } from '../../../libs';
+import { News, Menu } from '../../../models';
+
+module.exports = async (req, res, next) => {
+    try {
+
+        let { limit, page, skip } = req.query;
+        let { categoryName, type } = req.params;
+
+        let menu = await Menu.findOne()
+            .where('categoryName').equals(categoryName)
+            .execAsync();
+
+        // 如果連選單的資料都查不到，直接噴給他空的
+        if(!menu || !menu._id) {
+            throw new Error('11001');
+        }
+
+        let newsListCursor = News.find()
+            .where('isTrashed').equals(false)
+            .where('startedAt').lte(Date.now())
+            .where('status').equals('RELEASE')
+            .or([
+                { MainMenu: menu._id },
+                { Menus: menu._id}
+            ]);
+        let newsTotalCursor = News.find()
+            .where('isTrashed').equals(false)
+            .where('startedAt').lte(Date.now())
+            .where('status').equals('RELEASE')
+            .or([
+                { MainMenu: menu._id },
+                { Menus: menu._id}
+            ]);
+
+        if(type) {
+            newsListCursor.where('type').equals(type);
+            newsTotalCursor.where('type').equals(type);
+        }
+
+        let [ newsList, total ] = await Promise.all([
+            newsListCursor
+                .populate('MainMenu MainPhoto MainVideo')
+                .select('sn title shortTitle MainMenu MainPhoto MainVideo')
+                .sort('-startedAt')
+                .execAsync(),
+            newsTotalCursor.countAsync()
+        ]);
+
+        // 處理分頁
+        debug('total = %d', total);
+        let pageData = pagination(total, limit, page, skip);
+        debug('pageData = %j', pageData);
+
+        return res.json({
+            newsList,
+            pageData
+        });
+    } catch (err) {
+        return next(err);
+    }
+};
--- api-web/controllers/index.js
+++ api-web/controllers/index.js
@@ -1,10 +1,12 @@
 
+import category from './category';
 import news from './news';
 import menu from './menu';
 import preview from './preview';
 
 module.exports = (app) => {
 
+    app.use('/', category);
     app.use('/', news);
     app.use('/', menu);
     app.use('/', preview);
