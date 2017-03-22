78ce5e20c864c7764607daf008cc43f0af639a91
Simon Sun <blackghost0118@gmail.com>
2017-03-22 14:35:07 +0800

    :beetle: cors domaincors domain
    cors domain

--- api-web/middlewares/index.js
+++ api-web/middlewares/index.js
@@ -2,6 +2,7 @@
 import compression from 'compression';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import parseHeader from './parseHeader';
 
@@ -12,6 +13,7 @@ module.exports = (app) => {
     app.use(compression());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cors());
     app.use(logger('dev'));
 
     return (req, res, next) => {
