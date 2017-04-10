982a81f58180dbc5ac9d16f965538541c95a9f48
Simon Sun <blackghost0118@gmail.com>
2017-04-10 15:45:24 +0800

    :star2: search apisearch api
    search api

---
+++ api-web/controllers/search/list.js
@@ -0,0 +1,72 @@
+
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:api-web:controllers:search:list');
+
+import moment from 'moment-timezone';
+
+import { News } from '../../../models';
+import { pagination } from '../../../libs';
+
+module.exports = async (req, res, next) => {
+    try {
+
+        let { keyword } = req.params;
+        let { limit, skip, page, startedAt, endedAt } = req.query;
+
+        // 新聞相關的 cursor
+        let cursor = News.find();
+        let totalCursor = News.find();
+
+        // 如果有開始時間，就以開始時間為主，沒有的話就以現在時間為主
+        if(startedAt) {
+            startedAt = `moment(startedAt).format('YYYY-MM-DD') 00:00`;
+            cursor.where('startedAt').lte(moment(startedAt));
+            totalCursor.where('startedAt').lte(startedAt);
+        } else {
+            cursor.where('startedAt').lte(Date.now());
+            totalCursor.where('startedAt').lte(Date.now());
+        }
+
+        if(endedAt) {
+            endedAt = `moment(startedAt).format('YYYY-MM-DD') 23:59`;
+            cursor.where('startedAt').gte(endedAt);
+            totalCursor.where('startedAt').gte(endedAt);
+        }
+
+        // 找出相關列表與分頁資料
+        let [ newsList, total ] = await Promise.all([
+            cursor
+                .or([
+                    { title: new RegExp(keyword, 'i') },
+                    { content: new RegExp(keyword, 'i') }
+                ])
+                .where('isTrashed').equals(false)
+                .where('status').equals('RELEASE')
+                .populate('MainPhoto MainVideo')
+                .limit(limit)
+                .skip(skip)
+                .select('sn title MainPhoto content startedAt createdAt updatedAt')
+                .sort('-createdAt')
+                .execAsync(),
+            totalCursor
+                .or([
+                    { title: new RegExp(keyword, 'i') },
+                    { content: new RegExp(keyword, 'i') }
+                ])
+                .where('isTrashed').equals(false)
+                .where('status').equals('RELEASE')
+                .countAsync()
+        ]);
+        debug('news list = %j', newsList);
+
+        // 處理分頁
+        let pageData = pagination(total, limit, page, skip);
+
+        return res.json({
+            newsList,
+            pageData
+        });
+    } catch (err) {
+        return next(err);
+    }
+};
