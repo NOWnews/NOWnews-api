afc4e37826337819a9373c0940c8984d3d262c6c
WebberWu <bird23074035@gmail.com>
2017-04-18 15:07:56 +0800

    🔨 Refactor: simon is good!!!

--- api-admin/controllers/score/list.js
+++ api-admin/controllers/score/list.js
@@ -15,9 +15,9 @@ module.exports = async (req, res, next) => {
     try {
 
         let { startedAt, endedAt, sort, menuId } = req.query;
-        startedAt = startedAt ? `${startedAt} 00:00` : `${moment().format('YYYY-MM-DD')} 00:00`;
-        endedAt = endedAt ? `${endedAt} 23:59` : `${moment().format('YYYY-MM-DD')} 23:59`;
-        sort = sort || '-startedAt';
+        // startedAt = startedAt ? `${startedAt} 00:00` : `${moment().format('YYYY-MM-DD')} 00:00`;
+        // endedAt = endedAt ? `${endedAt} 23:59` : `${moment().format('YYYY-MM-DD')} 23:59`;
+        // sort = sort || '-startedAt';
 
         // 找出某個分類裡面的新聞
         let newsList = await News.find()
@@ -30,18 +30,27 @@ module.exports = async (req, res, next) => {
                 { Menus: menuId }
             ])
             .sort(sort)
-            .select('_id sn title')
+            .select('_id sn title shortTitle')
             .lean()
             .execAsync();
 
+        debug('newsList = %j', newsList);
 
         // 用新聞 id 找出 pv
         let newsListWithPageviews = await Promise.mapSeries(newsList, (news) => {
             return Pageview.findOne()
                 .where('newsId').equals(news._id)
                 .then((pageviewData) => {
-                    news.pageviews = pageviewData.pageviews;
-                    news.temperatures = pageviewData.temperatures;
+
+                    if (!pageviewData) {
+                        news.originalPageviews = 0;
+                        news.pageviews = 0;
+                        news.weightedScore = 0;
+                        news.totalScore = 0;
+                        return Promise.resolve(news);
+                    }
+                    news.pageviews = pageviewData.pageviews + pageviewData.temperatures;
+                    news.originalPageviews = pageviewData.pageviews;
                     news.weightedScore = pageviewData.weightedScore;
                     news.totalScore = pageviewData.totalScore;
                     return Promise.resolve(news);
