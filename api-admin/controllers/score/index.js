1acb722ca8d5b2729afe6ed719f3e5c38a277be0
Simon Sun <blackghost0118@gmail.com>
2017-04-24 11:17:43 +0800

    :star2: add update score endpointsadd update score endpoints
    add update score endpoints

--- api-admin/controllers/score/index.js
+++ api-admin/controllers/score/index.js
@@ -3,8 +3,12 @@ import express from 'express';
 let router = express.Router();
 
 import list from './list';
+import update from './update';
 
 router.route('/scores')
     .get(list);
+
+router.route('/scores/:newsId')
+    .put(update);
 
 module.exports = router;
