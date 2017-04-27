44412df5e76f006dd96443b36d1262c9e92fedc5
esbb48 <esbb48@gmail.com>
2017-04-27 17:16:55 +0800

    🔨 Refactor: Remove no need api

--- api-web/controllers/specialTopic/index.js
+++ api-web/controllers/specialTopic/index.js
@@ -5,12 +5,8 @@ let router = express.Router();
 import baseQuery from '../../middlewares/baseQuery';
 
 import list from './list';
-import one from './one';
 
 router.route('/specialtopics')
     .get(baseQuery, list);
-
-router.route('/specialtopics/:sn')
-    .get(one);
 
 module.exports = router;
