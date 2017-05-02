b4db6fa8a8058cfe6204555391b09538295e5ee5
esbb48 <esbb48@gmail.com>
2017-05-02 15:57:36 +0800

    ✨ Feat: Add deep populate

--- models/schemas/specialChannel.js
+++ models/schemas/specialChannel.js
@@ -1,8 +1,9 @@
-
+import DeepPopulate from 'mongoose-deep-populate';
 import autoIncrement from 'mongoose-easy-auto-increment';
 import mongoose from 'mongoose';
 import moment from 'moment-timezone';
 let Schema = mongoose.Schema;
+let deepPopulate = DeepPopulate(mongoose);
 
 let schema = new Schema({
 
@@ -83,5 +84,7 @@ schema.virtual('formatUpdatedAt').get(function () {
 });
 
 schema.plugin(autoIncrement);
+
+schema.plugin(deepPopulate);
 
 module.exports = schema;
