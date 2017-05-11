6efca6eeaf1571da2c138314b90a11926a17b605
HSIEH TZU HAO <jasonhsieh@HSIEHs-MacBook-Pro.local>
2017-05-11 18:17:10 +0800

    Fix: wrong name

--- api-web/controllers/category/list.js
+++ api-web/controllers/category/list.js
@@ -13,7 +13,7 @@ module.exports = async (req, res, next) => {
         let { categoryName, type } = req.params;
 
         let menu = await Menu.findOne()
-            .where('categoryName').equals(categoryName)
+            .where('name').equals(categoryName)
             .execAsync();
 
         // 如果連選單的資料都查不到，直接噴給他空的
