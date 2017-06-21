c5fffdf2affdc240587c852bce4c5846f13ae94b
esbb48 <esbb48@gmail.com>
2017-06-21 11:38:35 +0800

    ✨ Feat: Add try catch

--- libs/transformBig5.js
+++ libs/transformBig5.js
@@ -1,6 +1,14 @@
 import iconv from 'iconv-lite';
 
 module.exports = (ad) => {
-    const adString = iconv.decode(new Buffer(ad), 'BIG5');
-    return JSON.parse(adString);
+    try {
+        const adString = iconv.decode(new Buffer(ad), 'BIG5');
+        return JSON.parse(adString);
+    } catch (e) {
+        return {
+            img: '',
+            title: '',
+            url: ''
+        }
+    }
 };
