8a0de79d8c0e36f82edfb00378cca95074c954e2
JaosnHsieh <appleoxxo@gmail.com>
2017-06-30 16:28:29 +0800

    🐛 Fix: adjust rss end time condition

--- api-web/controllers/rss/list.js
+++ api-web/controllers/rss/list.js
@@ -38,7 +38,7 @@ module.exports = async(req, res, next) => {
             }
 
             if (end) {
-                end = moment.tz(end, 'Asia/Taipei').endOf('day');
+                end = Date.now();
                 debug('endTime %s', end );
                 cursor.where('startedAt').lte(end);
             }
