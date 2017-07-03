c1bbd7ae7f3dd51da164f31081d79310e7eeb71b
esbb48 <esbb48@gmail.com>
2017-07-03 11:50:17 +0800

    ✨ Feat: Update query factor

--- api-web/controllers/search/list.js
+++ api-web/controllers/search/list.js
@@ -41,12 +41,11 @@ module.exports = async (req, res, next) => {
         .where('isTrashed').equals(false);
 
         if (momentUnit) {
-            const tomorrow = moment.tz('Asia/Taipei').add(1, 'day').startOf('day');
             startedAt = moment.tz('Asia/Taipei').subtract(1, momentUnit).startOf('day');
             cursor.where('startedAt').gte(startedAt);
             totalCursor.where('startedAt').gte(startedAt);
-            cursor.where('startedAt').lt(tomorrow);
-            totalCursor.where('startedAt').lt(tomorrow);
+            cursor.where('startedAt').lte(Date.now());
+            totalCursor.where('startedAt').lte(Date.now());
         } else {
 
             // default StratedAt is today.
