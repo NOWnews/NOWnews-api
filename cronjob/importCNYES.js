052ca5963527c2dc3d167b7d1edace978fa44706
JaosnHsieh <appleoxxo@gmail.com>
2017-08-01 15:50:41 +0800

    🐛 Fix: add target _blank for cnyes news

--- cronjob/importCNYES.js
+++ cronjob/importCNYES.js
@@ -67,7 +67,8 @@ module.exports = new cron.CronJob({
 
                 //鉅亨網要求加上在新聞內文 文末加上連結
                 const link = "http://news.cnyes.com/news/cat/all?utm_medium=news&utm_source=nownews";
-                item['content:encoded'] +=`\n更多精彩內容請至 《鉅亨網》 <a href="${link}">連結>></a>`
+                item['content:encoded'] +=`\n更多精彩內容請至 《鉅亨網》 <a target="_blank" href="${link}">連結>></a>`
+
                 /*
                  * 鉅亨網沒有圖片，所以不用處理
                  * 這些處理圖片的 code 留下來當參考
