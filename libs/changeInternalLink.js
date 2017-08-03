9d4e9aba0e7b99bd4704b0554afbd7ee89e60d3e
JaosnHsieh <appleoxxo@gmail.com>
2017-08-03 12:01:17 +0800

    🐛 Fix: remove async

--- libs/changeInternalLink.js
+++ libs/changeInternalLink.js
@@ -2,7 +2,7 @@ import cheerio from 'cheerio';
 import Debug from 'debug';
 const debug = Debug('NOWnews-api:libs:changeInternalLink');
 
-module.exports = async (htmlContent) => {
+module.exports = (htmlContent) => {
     try {
           let $ = cheerio.load( htmlContent , { decodeEntities: false });
           $('a[href^="/"]').attr('href','/');
