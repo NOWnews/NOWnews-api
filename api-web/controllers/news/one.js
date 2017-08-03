ea47a99474194d74b2fa440b64084009968e2282
Webber <bird23074035@gmail.com>
2017-08-03 15:14:42 +0800

    ✨ Feat: 加上世大運的判斷依據

--- api-web/controllers/news/one.js
+++ api-web/controllers/news/one.js
@@ -6,6 +6,10 @@ import libs from '../../../libs';
 import { News } from '../../../models';
 import { Pageview } from '../../../pvModels'
 import _ from 'lodash';
+// TODO 為了世大運特別加的
+import uuidv4 from 'uuid/v4';
+import moment from 'moment-timezone';
+
 module.exports = async (req, res, next) => {
 
     try {
@@ -35,6 +39,68 @@ module.exports = async (req, res, next) => {
         }
         news = news.toJSON();
         news.pageView = { totalScore: 0 };
+
+        // TODO 為了世大運特別加的
+        let isWin = _.random(1, 10) === 1 ? true : false;
+        let kindList = [];
+        let kindProbability = (list, ballType, probability) => {
+            while (i < probability) {
+                list.push(ballType);
+                i++;
+            }
+            return list;
+        };
+        let isChannel = false;
+        switch(moment.tz('Asia/Taipei').format('YYYYMMDD')) {
+            case '20170814':
+            case '20170815':
+            case '20170816':
+            case '20170817':
+            case '20170818':
+            case '20170819':
+            case '20170820':
+                isChannel = ['運動', '生活', '娛樂'].indexOf(news.MainMenu.name) > -1;
+                kindList = kindProbability(kindList, 'basketball', 50);
+                kindList = kindProbability(kindList, 'baseball', 20);
+                kindList = kindProbability(kindList, 'tennis', 20);
+                break;
+            case '20170821':
+            case '20170822':
+            case '20170823':
+            case '20170824':
+            case '20170825':
+            case '20170826':
+            case '20170827':
+            case '20170828':
+            case '20170829':
+            case '20170830':
+                isChannel = ['運動', '政治', '社會'].indexOf(news.MainMenu.name) > -1;
+                kindList = kindProbability(kindList, 'volleyball', 50);
+                kindList = kindProbability(kindList, 'snooker', 50);
+                break;
+            case '20170831':
+            case '20170901':
+            case '20170902':
+                isChannel = true;
+                kind = ['basketball', 'baseball', 'tennis', 'volleyball', 'snooker'];
+                kindList = kindProbability(kindList, 'basketball', 20);
+                kindList = kindProbability(kindList, 'baseball', 20);
+                kindList = kindProbability(kindList, 'tennis', 30);
+                kindList = kindProbability(kindList, 'volleyball', 15);
+                kindList = kindProbability(kindList, 'snooker', 15);
+                break;
+        }
+        if (isChannel && isWin) {
+            let id = uuidv4();
+            let createdAt = moment.tz('Asia/Taipei').format('YYYYMMDDHHmm');
+            news.SummerUniversiade = {
+                id,
+                createdAt,
+                kind: kindList[_.random(kindList.length - 1)]
+            };
+        }
+        //--- TODO END
+
 
         //加上pageview的totalscore
         let pageviewList = await Pageview.find()
