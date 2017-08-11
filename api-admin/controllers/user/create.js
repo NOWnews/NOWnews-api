d932a7389a6b2de31ccb0f52db4c898f3c8f38f6
Webber <bird23074035@gmail.com>
2017-08-11 19:54:15 +0800

    ✨ Feat: update

--- api-admin/controllers/user/create.js
+++ api-admin/controllers/user/create.js
@@ -40,7 +40,7 @@ module.exports = async (req, res, next) => {
         }
 
         if(defaultLocation) {
-            uoptions.defaultSettings.location = defaultLocation;
+            options.defaultSettings.location = defaultLocation;
         }
 
         if (defaultMenu && mongoose.Types.ObjectId.isValid(defaultMenu)) {
