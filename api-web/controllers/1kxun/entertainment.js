5bde12096962f8ad1368d1ced1a57377382c9c43
Simon Sun <blackghost0118@gmail.com>
2017-08-15 12:01:23 +0800

    :star2: add new api point for 1kxun

---
+++ api-web/controllers/1kxun/entertainment.js
@@ -0,0 +1,42 @@
+
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:api-web:controllers:1kxun:entertainment');
+
+import _ from 'lodash';
+import moment from 'moment-timezone';
+
+import { News } from '../../../models';
+
+const entertainmentObjId = '560000000000000000000006';
+
+module.exports = async (req, res, next) => {
+    try {
+
+        let newsList = await News.find()
+            .where('MainMenu').equals(entertainmentObjId)
+            .where('status').equals('RELEASE')
+            .where('isTrashed').equals(false)
+            .where('startedAt').lte(Date.now())
+            .limit(50)
+            .select('sn title shortTitle summary startedAt')
+            .sort('-startedAt')
+            .lean()
+            .execAsync();
+
+        // 整理相關資訊
+        newsList = _.map(newsList, (news) => {
+            return {
+                title: news.title,
+                shortTitle: news.shortTitle,
+                summary: news.summary,
+                startedAt: moment(news.startedAt).unix('x'),
+                url: `https://www.nownews.com/news/${moment.tz(news.startedAt, 'Asia/Taipei').format('YYYYMMDD')}/${news.sn}`
+            };
+        });
+        debug('news list = %j', newsList);
+
+        return res.json(newsList);
+    } catch (err) {
+        return next(err);
+    }
+};
