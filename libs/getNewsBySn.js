c37a0be11c352f2b14b7424da6d85a78cfcfeed7
Simon Sun <blackghost0118@gmail.com>
2017-08-23 10:50:24 +0800

    add Author field

--- libs/getNewsBySn.js
+++ libs/getNewsBySn.js
@@ -39,11 +39,11 @@ module.exports = async (sn) => {
                 },
                 {
                     path: 'Author',
-                    populate: { path: 'Avatar', select: 'url' },
-                    select: 'Avatar'
+                    populate: { path: 'Avatar' },
+                    select: 'Avatar _id'
                 }
             ])
-            .select('_id sn title templateAD template Tags newsBy isSponsored isAdult traceCode type startedAt freeContent Photos content MainVideo MainPhoto Menus MainMenu summary shortTitle')
+            .select('_id sn title templateAD template Tags newsBy isSponsored isAdult traceCode type startedAt freeContent Photos content MainVideo MainPhoto Menus MainMenu summary shortTitle Author')
             .execAsync();
         debug('news = %j', news);
 
