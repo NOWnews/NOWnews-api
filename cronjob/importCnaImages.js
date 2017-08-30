048559015c3f74470c758e8a3427603118a1f793
JaosnHsieh <appleoxxo@gmail.com>
2017-08-30 18:36:57 +0800

    🐛 Fix: revise according to @wb comment

--- cronjob/importCnaImages.js
+++ cronjob/importCnaImages.js
@@ -8,7 +8,6 @@ const debug = Debug('NOWnews-api:cron:cronjob:importCnaImages');
 import config from 'config';
 import cron from 'cron';
 import _ from 'lodash';
-import moment from 'moment-timezone';
 import { parseRssFeed } from '../libs';
 import { Image } from '../models';
 module.exports = new cron.CronJob({
@@ -27,58 +26,59 @@ module.exports = new cron.CronJob({
             }
 
             let rssJSON = await parseRssFeed(feedUrl);
+            //因中央社圖片XML的結構會依時間有變更 所以這樣指定值 詳見NOWnews-api Gitgub issue #652
             let imageList =
                 _.get(rssJSON, 'NewsML.NewsItem[0].NewsComponent.NewsComponent.NewsComponent', null) ||
                 _.get(rssJSON, 'NewsML.NewsItem.NewsComponent.NewsComponent.NewsComponent', null);
 
             if(!imageList){
-              console.error('中央社團片 解析XML後沒有圖片列表！！');
-              return;
+                console.error('中央社團片 解析XML後沒有圖片列表！！');
+                return;
             }
 
             debug('中央社 imageList = %j', imageList);
 
             for(let image of  imageList){
-                  let href = _.get(image, 'NewsComponent.NewsComponent[0].ContentItem.Href', null);
-                  let title = _.get(image, 'NewsComponent.NewsComponent[1].ContentItem.DataContent.p[0]', '');
-                  let desc = _.get(image, 'NewsComponent.NewsComponent[1].ContentItem.DataContent.p[1]', '');
-                  let mimeType = _.get(image, 'NewsComponent.NewsComponent[0].ContentItem.MimeType.FormalName', null);
-                  // 如果xml結構找出來沒有href 就不收錄
-                  if(!href){
-                    continue;
-                  }
-                  // 如果這張圖片已經存過了 之後的就都不收錄 因為這資料有按照順序時間排
-                  let aliveImage = await Image.findOne()
-                      .where('url').equals(href)
-                      .execAsync();
-                  if(aliveImage) {
-                    break;
-                  }
+                let href = _.get(image, 'NewsComponent.NewsComponent[0].ContentItem.Href', null);
+                let title = _.get(image, 'NewsComponent.NewsComponent[1].ContentItem.DataContent.p[0]', '');
+                let desc = _.get(image, 'NewsComponent.NewsComponent[1].ContentItem.DataContent.p[1]', '');
+                let mimeType = _.get(image, 'NewsComponent.NewsComponent[0].ContentItem.MimeType.FormalName', null);
+                // 如果xml結構找出來沒有href 就不收錄
+                if(!href){
+                  continue;
+                }
+                // 如果這張圖片已經存過了 之後的就都不收錄 因為這資料有按照順序時間排
+                let aliveImage = await Image.findOne()
+                    .where('url').equals(href)
+                    .execAsync();
+                if(aliveImage) {
+                  break;
+                }
 
-                  let imageOptions = {
-                      title: title,
-                      desc: desc,
-                      keyword: '中央社',
-                      imageFrom: 'CNA',
-                      originalname: null,
-                      format: null,
-                      type: 'NEWS',
-                      mode: 'NORMAl',
-                      mimetype: mimeType,
-                      width: null,
-                      height: null,
-                      isDeliver: false,
-                      Tag: null,
-                      url: href,
-                      isTrashed: false,
-                      CreatedBy: '530000000000000000000004',
-                      UpdatedBy: '530000000000000000000004',
-                  };
+                let imageOptions = {
+                    title: title,
+                    desc: desc,
+                    keyword: '中央社',
+                    imageFrom: 'CNA',
+                    originalname: null,
+                    format: null,
+                    type: 'NEWS',
+                    mode: 'NORMAl',
+                    mimetype: mimeType,
+                    width: null,
+                    height: null,
+                    isDeliver: false,
+                    Tag: null,
+                    url: href,
+                    isTrashed: false,
+                    CreatedBy: '530000000000000000000004',
+                    UpdatedBy: '530000000000000000000004',
+                };
 
-                  image = await Image.createAsync(imageOptions);
-                  console.log(`匯入中央社圖片URL: ${href}`);
-                  console.log(`匯入中央社圖片title: ${title}`);
-                  console.log(`-------------------------------------------`);
+                image = await Image.createAsync(imageOptions);
+                console.log(`匯入中央社圖片URL: ${href}`);
+                console.log(`匯入中央社圖片title: ${title}`);
+                console.log(`-------------------------------------------`);
             }
 
             console.log(`------------- Finish Import 中央社 圖片 -------------`);
