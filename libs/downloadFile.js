45f202de56058cbec528a7a19d044af59478b3af
JaosnHsieh <appleoxxo@gmail.com>
2017-09-02 15:03:52 +0800

    ✨ Feat: add a downloadFile in libs

---
+++ libs/downloadFile.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import fs from 'fs';
+import Promise from 'bluebird';
+import path from 'path';
+import uuidv4 from 'uuid/v4';
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:libs:downloadFile');
+
+module.exports = (url) => {
+    return new Promise((resolve, reject) => {
+        let fileDest = path.join('uploads');
+        let fileName = uuidv4();
+        let filePath = path.join(fileDest, fileName);
+        //http://rss.cna.com.tw/client/nownews/pho/photo/20170831/20170831000001M.jpg
+        axios.get(url, {
+            responseType: 'arraybuffer'
+        }).then(response => {
+            fs.writeFile(filePath, new Buffer(response.data, 'binary'), "binary", err => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(filePath);
+                }
+            });
+        }).catch(err => {
+            return reject(err);
+        });
+    })
+};
