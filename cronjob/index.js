932f29038939cf4d116ad62a6cb9dcba03fafa95
JaosnHsieh <appleoxxo@gmail.com>
2017-09-02 17:35:13 +0800

    🐛 Fix: resume comment

--- cronjob/index.js
+++ cronjob/index.js
@@ -1,12 +1,11 @@
-
-// import updateAllHotNews from './updateAllHotNews';
-// import updatePersonalize from './updatePersonalize';
-// import updateHotKeywords from './updateHotKeywords';
-// import updateInstantNews from './updateInstantNews';
-// import updateFirstPageForMobile from './updateFirstPageForMobile';
-// import updateFirstPageForDesktop from './updateFirstPageForDesktop';
-// import updateNewestNews from './updateNewestNews';
-// import updateNewestImages from './updateNewestImages';
+import updateAllHotNews from './updateAllHotNews';
+import updatePersonalize from './updatePersonalize';
+import updateHotKeywords from './updateHotKeywords';
+import updateInstantNews from './updateInstantNews';
+import updateFirstPageForMobile from './updateFirstPageForMobile';
+import updateFirstPageForDesktop from './updateFirstPageForDesktop';
+import updateNewestNews from './updateNewestNews';
+import updateNewestImages from './updateNewestImages';
 // import importCNYES from './importCNYES';
 import importCnaImages from './importCnaImages';
 
@@ -14,14 +13,14 @@ import importCnaImages from './importCnaImages';
 module.exports = async () => {
 
     await Promise.all([
-        // updateAllHotNews.start(),
-        // updatePersonalize.start(),
-        // updateHotKeywords.start(),
-        // updateInstantNews.start(),
-        // updateFirstPageForMobile.start(),
-        // updateFirstPageForDesktop.start(),
-        // updateNewestNews.start(),
-        // updateNewestImages.start(),
+        updateAllHotNews.start(),
+        updatePersonalize.start(),
+        updateHotKeywords.start(),
+        updateInstantNews.start(),
+        updateFirstPageForMobile.start(),
+        updateFirstPageForDesktop.start(),
+        updateNewestNews.start(),
+        updateNewestImages.start(),
         // importCNYES.start(),
         importCnaImages.start()
     ]);
