cbfad88fa0e784c2594157ac08be035feae15416
JaosnHsieh <appleoxxo@gmail.com>
2017-09-04 18:30:03 +0800

    🐛 Fix: resume cnyes news import

--- cronjob/index.js
+++ cronjob/index.js
@@ -7,7 +7,7 @@ import updateFirstPageForMobile from './updateFirstPageForMobile';
 import updateFirstPageForDesktop from './updateFirstPageForDesktop';
 import updateNewestNews from './updateNewestNews';
 import updateNewestImages from './updateNewestImages';
-// import importCNYES from './importCNYES';
+import importCNYES from './importCNYES';
 import importCnaImages from './importCnaImages';
 
 
@@ -22,7 +22,7 @@ module.exports = async () => {
         updateFirstPageForDesktop.start(),
         updateNewestNews.start(),
         updateNewestImages.start(),
-        // importCNYES.start(),
+        importCNYES.start(),
         importCnaImages.start()
     ]);
 };
