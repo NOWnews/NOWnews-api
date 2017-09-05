88fe6eb97aef3aed1dd0c2d3831a97af28dc5928
JaosnHsieh <appleoxxo@gmail.com>
2017-09-05 14:13:10 +0800

    🐛 Fix: add init data when run cronjob

--- bin/cron.js
+++ bin/cron.js
@@ -1,7 +1,14 @@
 require('babel-core/register');
 require('babel-polyfill');
 
-const cronjobs = require('../cronjob');
+let initDataStart = require('../initData/start');
 
-cronjobs();
-console.log('cron job start');
+// 初始化資料
+initDataStart()
+    .then(result => {
+        const cronjobs = require('../cronjob');
+        cronjobs();
+        console.log('cron job start');
+    }).catch(err => {
+        console.error('初始化資料失敗', err);
+    });
