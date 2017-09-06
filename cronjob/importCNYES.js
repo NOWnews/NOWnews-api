9acf08a112daccc6159bbbe2388f3bf804b92482
JaosnHsieh <appleoxxo@gmail.com>
2017-09-06 15:51:44 +0800

    🐛 Fix: change CNYES news mainMenu

--- cronjob/importCNYES.js
+++ cronjob/importCNYES.js
@@ -147,7 +147,7 @@ module.exports = new cron.CronJob({
                     location: [121.5914087,25.0693482], //台北市內湖區的座標
                     shortTitle: shortTitle,
                     summary: title, //鉅亨網沒有提供summary這個欄位 但前台og tag要用到summary 所以放title
-                    MainMenu: '560000000000000000000016',
+                    MainMenu: '560000000000000000000013',
                     Menus: ['560000000000000000000013'],
                     MainPhoto: randomDefaultImageId,
                     MainVideo: null,
