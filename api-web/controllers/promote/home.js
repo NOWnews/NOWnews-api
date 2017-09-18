7c3813735d289f40835d4abd1ffafe71eaed585c
ALi Li <esbb48@users.noreply.github.com>
2017-09-18 16:30:08 +0800

    Update home.js

--- api-web/controllers/promote/home.js
+++ api-web/controllers/promote/home.js
@@ -12,7 +12,6 @@ module.exports = async (req, res, next) => {
 
         const redisValue = await redis.getValue('adWebHome');
 
-        // 依據每次新增的內容判斷是某要更新
         if (redisValue && !!redisValue.crazyAd) {
             return res.json(redisValue);
         }
