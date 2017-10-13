55a046837acc3becbe595a864b1544818acb23df
wayne1025 <wayne1025.tw@hotmail.com>
2017-10-13 16:35:45 +0800

    🔨 Refactor: add android images size

--- libs/prepareImages.js
+++ libs/prepareImages.js
@@ -9,15 +9,25 @@ module.exports = async (news) => {
 
         // 先讓 img.nownews.com 有 cache
         await axios.get(news.MainPhoto.url);
-
+        //android尺寸 1440 1080 720 540 360
+        //桌機尺寸 1080 300 200
+        //手機尺寸 640 300
         // 讓 imagelab 有 cache
         await Promise.all([
+            axios.get(`http://35.201.178.13:8877/?w=1440&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.178.13:8877/?w=1080&q=85&src=${news.MainPhoto.url}`),
+            axios.get(`http://35.201.178.13:8877/?w=720&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.178.13:8877/?w=640&q=70&src=${news.MainPhoto.url}`),
+            axios.get(`http://35.201.178.13:8877/?w=540&q=70&src=${news.MainPhoto.url}`),
+            axios.get(`http://35.201.178.13:8877/?w=360&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.178.13:8877/?w=300&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.178.13:8877/?w=200&q=70&src=${news.MainPhoto.url}`),
+            axios.get(`http://35.201.178.15:8877/?w=1440&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.162.15:8877/?w=1080&q=85&src=${news.MainPhoto.url}`),
+            axios.get(`http://35.201.178.15:8877/?w=720&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.162.15:8877/?w=640&q=70&src=${news.MainPhoto.url}`),
+            axios.get(`http://35.201.178.15:8877/?w=540&q=70&src=${news.MainPhoto.url}`),
+            axios.get(`http://35.201.178.15:8877/?w=360&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.162.15:8877/?w=300&q=70&src=${news.MainPhoto.url}`),
             axios.get(`http://35.201.162.15:8877/?w=200&q=70&src=${news.MainPhoto.url}`)
         ]);
