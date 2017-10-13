0a258034126c7be10062289655927eeb444d7b0a
esbb48 <esbb48@gmail.com>
2017-10-13 10:57:43 +0800

    ✨ Feat: Add develop mode fcm config

--- api-admin/middlewares/prepare.js
+++ api-admin/middlewares/prepare.js
@@ -1,14 +1,18 @@
 
 import firebaseAdmin from 'firebase-admin';
-import serviceAccount from '../../pems/NOWnews-project-firebase.json';
+import serviceAccountProd from '../../pems/NOWnews-project-firebase-prod.json';
+import serviceAccountDev from '../../pems/NOWnews-project-firebase-dev.json';
 import chalk from 'chalk';
 
 module.exports = () => {
+	const isProd = process.env.NODE_ENV === 'production';
+	const serviceAccount = isProd ? serviceAccountProd : serviceAccountDev;
+	const databaseURL = isProd ? 'https://spry-smithy-96510.firebaseio.com' : 'https://nownews-website-167108.firebaseio.com';
 
     // init firebase
     firebaseAdmin.initializeApp({
         credential: firebaseAdmin.credential.cert(serviceAccount),
-        databaseURL: 'https://spry-smithy-96510.firebaseio.com'
+        databaseURL,
     });
     console.log(chalk.yellow('Firebase initializeApp done.'))
 
