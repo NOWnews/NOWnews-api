aa2dc7a28b90741e9bb6551462b140f91ed61d70
JaosnHsieh <appleoxxo@gmail.com>
2017-10-24 18:59:18 +0800

    🐛 Fix: add wrong deleted code back

--- api-admin/controllers/releaseRule/list.js
+++ api-admin/controllers/releaseRule/list.js
@@ -1,5 +1,6 @@
 import Debug from 'debug';
 const debug = Debug('NOWnews-api:api-admin:controllers:releaseRule:list');
+import { ReleaseRule } from '../../../models';
 module.exports = async (req, res, next) => {
     try {
         let releaseRules = await ReleaseRule
