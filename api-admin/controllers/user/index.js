663ca7c74f9ecdca7a57546c94ca122c95215524
JaosnHsieh <appleoxxo@gmail.com>
2017-10-27 18:42:29 +0800

    🐛 Fix: move /role/{id}/reviewers to  /user/{id}/reviewers api and add releaseRules

--- api-admin/controllers/user/index.js
+++ api-admin/controllers/user/index.js
@@ -12,6 +12,7 @@ import update from './update';
 import remove from './remove';
 import login from './login';
 import logout from './logout';
+import reviewers from './reviewers';
 
 router.route('/users')
     .post(validators.user.create, create)
@@ -27,5 +28,8 @@ router.route('/users/:id')
     .get(one)
     .put(validators.user.update, update)
     .delete(remove);
+
+router.route('/users/:id/reviewers')
+    .get(reviewers);
 
 module.exports = router;
---
+++ api-admin/controllers/user/reviewers.js
@@ -0,0 +1,74 @@
+import Debug from 'debug';
+const debug = Debug('NOWnews-api:api-admin:controllers:user:reviewrs');
+import { Role, User, ReleaseRule } from '../../../models';
+import _ from 'lodash';
+
+module.exports = async(req, res, next) => {
+
+    let { id } = req.params;
+
+    try {
+        const user = await User.findOne()
+            .where('_id').equals(id)
+            .where('isTrashed').equals(false)
+            .select('id Role Center')
+            .populate('Role');
+
+        if (!user) {
+            throw new Error('11011');
+        }
+
+        let cursor = User.find();
+        let releaseRules = await ReleaseRule
+            .findOne({})
+            .sort({
+                createdAt: -1
+            });
+        if (releaseRules) {
+            let rules = releaseRules.rules;
+            if (rules.excludeRoles.isOn) {
+                cursor.where('Role').nin(rules.excludeRoles.setting.roleIds);
+            }
+            if (rules.sameCenter.isOn) {
+                cursor.or({
+                    'Center': user.Center.toString()
+                });
+            }
+            rules.sameUser.isOn ?
+                cursor.where('_id').ne(user.id) : cursor.or({ '_id': user.id });
+
+            if (rules.timeAndRole.isOn) {
+                let setting = rules.timeAndRole.setting;
+                _.forEach(setting, (s) => {
+                    cursor.or([{
+                        $and: [{
+                                'Role': {
+                                    $in: s.roleIds
+                                }
+                            },
+                            {
+                                'Center': s.centerId
+                            }
+                        ]
+                    }]);
+                });
+            }
+        }
+
+        const users = await cursor.find()
+            .where('isTrashed').equals(false)
+            .where('status').equals('REGULAR')
+            .select('_id name')
+            .or({
+                'Role': {
+                    $in: user.Role.SuperiorRoles
+                }
+            })
+            .execAsync();
+
+        return res.json(users);
+
+    } catch (err) {
+        return next(err);
+    };
+};
