ab350ff1b15c572d882e203b9a2759e2b4e73aa4
esbb48 <esbb48@gmail.com>
2017-10-30 15:07:40 +0800

    ✨ Feat: Prevent pre-release news

--- api-admin/controllers/news/oneBySn.js
+++ api-admin/controllers/news/oneBySn.js
@@ -11,12 +11,15 @@ module.exports = async (req, res, next) => {
 
         const news = await News.findOne()
             .where('sn').equals(sn)
+            .where('startedAt').lte(Date.now())
             .where('isTrashed').equals(false)
             .populate('MainPhoto')
             .execAsync();
+
         if(!news) {
             throw new Error('16003');
         }
+        
         return res.json(news);
     }catch(err) {
         return next(err);
