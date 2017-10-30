ed1c98b48494489510db4d00709a18a92070a25b
esbb48 <esbb48@gmail.com>
2017-10-30 15:33:05 +0800

    🔨 Refactor: Use await libs.getNewsBySn(sn);

--- api-admin/controllers/news/oneBySn.js
+++ api-admin/controllers/news/oneBySn.js
@@ -1,20 +1,14 @@
 import Debug from 'debug';
 const debug = Debug('NOWnews-api:api-admin:controllers:news:oneBySn');
 
-import { News } from '../../../models';
+import libs from '../../../libs';
 
 module.exports = async (req, res, next) => {
 
     const { sn } = req.params;
 
     try {
-
-        const news = await News.findOne()
-            .where('sn').equals(sn)
-            .where('startedAt').lte(Date.now())
-            .where('isTrashed').equals(false)
-            .populate('MainPhoto')
-            .execAsync();
+        const news = await libs.getNewsBySn(sn);
 
         if(!news) {
             throw new Error('16003');
