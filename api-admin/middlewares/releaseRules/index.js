e4af2a8424d6c57898787f6415847ec3b04542ef
JaosnHsieh <appleoxxo@gmail.com>
2017-10-30 15:38:48 +0800

    📚 Docs: add comment on releaseRule router file

--- api-admin/middlewares/releaseRules/index.js
+++ api-admin/middlewares/releaseRules/index.js
@@ -5,7 +5,13 @@ import superiorRoles from './superiorRoles';
 import timeAndRole from './timeAndRole';
 import excludeRoles from './excludeRoles'
 
-// middlewares array 有順序
+/** middlewaresArray 有順序
+ * 順序的依據是 當規則發生衝突時，愈前面的規則優先序最高
+ * getData 是先取得需要的資料
+ * 規則目前的順序 excludeRoles 最高
+ * 也就是說：當 excludeRoles 和 timeAndRole 的設定發生衝突時 會以 excludeRoles 的設定為準 以此類推
+ */
+
 let middlewaresArray = [
     getData,
     excludeRoles,
