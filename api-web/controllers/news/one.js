d5bcf2c9dff1491da050f242f328dd16daeadf35
wayne1025 <wayne1025.tw@hotmail.com>
2017-11-20 18:30:37 +0800

    修改內文圖片圖說by 兩種模式

--- api-web/controllers/news/one.js
+++ api-web/controllers/news/one.js
@@ -1,6 +1,6 @@
 import Debug from 'debug';
 const debug = Debug('NOWnews-api:api-web:controllers:news:one');
-
+import cheerio from 'cheerio';
 import redis from '../../../redis';
 import libs from '../../../libs';
 import { News } from '../../../models';
@@ -13,20 +13,20 @@ module.exports = async (req, res, next) => {
 
         let { sn } = req.params;
 
-        let cacheNews = await redis.getValue(`news${sn}`);
-        if(cacheNews) {
-            cacheNews.pageView = { totalScore : 0 };
-            //加上pageview的totalscore
-            let pageviewList = await Pageview.find()
-                .where('newsId').in(cacheNews.id)
-                .select('totalScore')
-                .execAsync();
-            for(let pv of pageviewList){
-                cacheNews.pageView.totalScore += pv.totalScore;
-            };
+        // let cacheNews = await redis.getValue(`news${sn}`);
+        // if(cacheNews) {
+        //     cacheNews.pageView = { totalScore : 0 };
+        //     //加上pageview的totalscore
+        //     let pageviewList = await Pageview.find()
+        //         .where('newsId').in(cacheNews.id)
+        //         .select('totalScore')
+        //         .execAsync();
+        //     for(let pv of pageviewList){
+        //         cacheNews.pageView.totalScore += pv.totalScore;
+        //     };
 
-            return res.json(cacheNews);
-        }
+        //     return res.json(cacheNews);
+        // }
 
         // 要給 api web 使用的 news 資料
         let news = await libs.getNewsBySn(sn);
@@ -41,7 +41,7 @@ module.exports = async (req, res, next) => {
 
         // 目前 IOS App 沒辦法吃 imgapi 的圖
         news.content = news.content.replace(/http(?:s?):\/\/(?:imgapiv2\.|web\.|m\.)?nownews\.com\/(?:.+src=)/g, '');
-
+        console.log(news.content)
         //加上pageview的totalscore
         let pageviewList = await Pageview.find()
             .where('newsId').in(news.id)
@@ -58,7 +58,17 @@ module.exports = async (req, res, next) => {
             const insertIndex = news.content.indexOf ('</p>', 250) + 4;
             news.contentAdIndex = insertIndex;
         }
+        const $ = cheerio.load(news.content, { decodeEntities: false });
+        //過濾內文圖片下一個p tag加上class
+        if($('img').parent('p').text().indexOf('\u25b2') != -1){
+            $('img').parent('p').addClass('imgdesc');
+        }
+        
+        if($('img').parent('p').nextAll('p').eq(0).text().indexOf('\u25b2') != -1){
+            $('img').parent('p').nextAll('p').eq(0).addClass('imgdesc');
+        }
 
+        news.content = $.html();
         // 將這篇新聞存入 redis
         let cacheData = await redis.setValue(`news${sn}`, news, 3600 * 6);
         debug('cacheData = %j', cacheData);
