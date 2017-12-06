7e16d528c639658bbe7d443fa47caa5141e688bb
Webber <bird23074035@gmail.com>
2017-12-06 16:56:39 +0800

    🔨 Refactor: 手動存進去以後 會將 redis 資料清除

--- api-admin/controllers/indexPage/addCarousels.js
+++ api-admin/controllers/indexPage/addCarousels.js
@@ -28,9 +28,8 @@ module.exports = async (req, res, next) => {
         let updatedIndexPage = await indexPage.saveAsync();
         debug('update indexPage = %j', updatedIndexPage);
 
-        // 將首頁資訊存入 redis
-        let cacheData = await libs.getIndexPage();
-        let cacheIndexPage = await redis.setValue('indexPage', cacheData);
+        // 將 redis 清除
+        redis.removeValue(`indexPage`);
 
         return res.json(updatedIndexPage);
     } catch (err) {
