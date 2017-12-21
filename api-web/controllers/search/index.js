427443f0870aaf3b9f149e6fb089f65db850b45f
esbb48 <esbb48@gmail.com>
2017-12-21 10:34:05 +0800

    🐛 Fix: Fix endpoint

--- api-web/controllers/search/index.js
+++ api-web/controllers/search/index.js
@@ -7,11 +7,10 @@ import baseQuery from '../../middlewares/baseQuery';
 import list from './list';
 import elasticsearch from './elasticsearch';
 
+// router.route('/search/:keyword')
+//     .get(baseQuery, list);
+
 router.route('/search/:keyword')
-    .get(baseQuery, list);
-
-
-router.route('/esearch/:keyword')
     .get(baseQuery, elasticsearch);
 
 module.exports = router;
