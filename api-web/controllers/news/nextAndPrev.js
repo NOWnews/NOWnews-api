9a599becdc56a73365111355e5507d6cf48e63a9
esbb48 <esbb48@gmail.com>
2017-12-28 16:20:53 +0800

    🐛 Fix: Fix next&prev

--- api-web/controllers/news/nextAndPrev.js
+++ api-web/controllers/news/nextAndPrev.js
@@ -37,20 +37,25 @@ module.exports = async (req, res, next) => {
             expire = 3600 * 5;
         }
 
+        const next = nextNews && {
+            sn: nextNews.sn,
+            title: nextNews.title,
+            shortTitle: nextNews.shortTitle,
+            parseUrl: nextNews.parseUrl
+        } || {};
+
+        const prev = prevNews && {
+            sn: prevNews.sn,
+            title: prevNews.title,
+            shortTitle: prevNews.shortTitle,
+            parseUrl: prevNews.parseUrl
+        } || {};
+        
+
         // 將這篇新聞存入 redis
         let newCache = await redis.setValue(`news${sn}NextAndPrev`, {
-            next: {
-                sn: nextNews.sn,
-                title: nextNews.title,
-                shortTitle: nextNews.shortTitle,
-                parseUrl: nextNews.parseUrl
-            },
-            prev: {
-                sn: prevNews.sn,
-                title: prevNews.title,
-                shortTitle: prevNews.shortTitle,
-                parseUrl: prevNews.parseUrl
-            }
+            next,
+            prev
         }, expire);
         debug('newCache = %j', newCache);
 
