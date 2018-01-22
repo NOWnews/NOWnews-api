10659cfa74e41bd9d8661d61ec8423ab65356c27
wayne1025 <wayne1025.tw@hotmail.com>
2018-01-22 16:00:58 +0800

    修改api live info

--- api-web/controllers/live/info.js
+++ api-web/controllers/live/info.js
@@ -1,24 +1,30 @@
 
 import Debug from 'debug';
+import moment from 'moment-timezone';
 const debug = Debug('NOWnews-api:api-web:controllers:live:info');
 
 module.exports = async (req, res, next) => {
     try {
-        // 金曲
+        let now = moment.tz('Asia/Taipei');
+        //NOW直播
+        
         return res.json({
-            title: '第28屆金曲獎頒獎典禮紅毯 ！#NOW直擊 媒體採訪區',
-            teaserTitle: '第28屆金曲獎頒獎典禮紅毯 ！#NOW直擊 媒體採訪區',
-            banner: 'http://legacy.nownews.com/NOWnews_static/live-banner.jpg',
-            alt: '第28屆金曲獎頒獎典禮紅毯 ！#NOW直擊 媒體採訪區',
+            title: '#NOW直播　常豐居士－夏唯綱教授 命理風水學現場直播',
+            teaserTitle: '#NOW直播　常豐居士－夏唯綱教授 命理風水學現場直播',
+            banner: 'http://legacy.nownews.com/NOWnews_static/live-banner.jpg?' + now,
+            alt: '#NOW直播　常豐居士－夏唯綱教授 命理風水學現場直播',
             url: 'https://www.youtube.com/embed/rqESGX2vt3M',
-            wowza: '',
+            wowza: 'https://5a5ab4e1760b0.streamlock.net/live/c000.stream/playlist.m3u8?pf=mm',
             youtubeId: 'rqESGX2vt3M',
+            liveUrl: 'Livefungshui',
             livePage: 'https://m.nownews.com/live/rqESGX2vt3M',
-            background: 'https://legacy.nownews.com/NOWnews_static/live-background.jpg',
+            background: 'https://legacy.nownews.com/NOWnews_static/live-background.jpg?' +  now,
             backgroundColor: '#403534',
             redirect: '',
             isOnAir: false,
-            campainStatus: false
+            campainStatus: false,
+            isOnAirforFungshui: true,
+            campainStatusforFungshui: true
         });
     } catch(err) {
         return next(err);
