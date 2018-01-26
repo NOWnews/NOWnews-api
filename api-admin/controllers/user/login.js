bf59ee84a9e4cf639bd45648c83cb0ea7f04fe57
esbb48 <esbb48@gmail.com>
2018-01-26 11:19:45 +0800

    ✨ Feat: refactor

--- api-admin/controllers/user/login.js
+++ api-admin/controllers/user/login.js
@@ -35,14 +35,13 @@ module.exports = async (req, res, next) => {
 
         let now = Date.now();
 
-        await Promise.all([
-            LoginTrack.createAsync({
-                User: user._id,
-                action: 'LOGIN',
-                createdAt: now,
-                ip
-            })
-        ]);
+        await LoginTrack.createAsync({
+            User: user._id,
+            action: 'LOGIN',
+            createdAt: now,
+            ip
+        });
+        
         return res.json(user);
     } catch (err) {
         return next(err);
