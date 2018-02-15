dc13998c18c21b548e2a47eb75873bdc7bc2e7dd
Leo Chang <leomax0725@gmail.com>
2018-02-15 10:21:47 +0800

    修改title

--- api-web/controllers/live/info.js
+++ api-web/controllers/live/info.js
@@ -8,10 +8,10 @@ module.exports = async (req, res, next) => {
         let now = moment.tz('Asia/Taipei');
         //NOW直播
         return res.json({
-            title: '#NOW直播 2018央視春晚現場直播',
-            teaserTitle: '#NOW直播 2018央視春晚現場直播',
+            title: '#NOW直播 2018央視春晚現場直播 20:00-00:30',
+            teaserTitle: '#NOW直播 2018央視春晚現場直播 20:00-00:30',
             banner: 'https://img.nownews.com/photo/festival_640.jpg?' + now,
-            alt: '#NOW直播 2018央視春晚現場直播',
+            alt: '#NOW直播 2018央視春晚現場直播 20:00-00:30',
             url: '',
             wowza: 'https://5a5ab4e1760b0.streamlock.net/live/c000.stream/playlist.m3u8?pf=mm',
             youtubeId: '',
