340426b8016eb37fe12f14d596446241daaf4b5b
Leo Chang <leomax0725@gmail.com>
2018-02-23 17:16:49 +0800

    修改專題順序

--- api-web/controllers/specialChannel/list.js
+++ api-web/controllers/specialChannel/list.js
@@ -20,7 +20,7 @@ module.exports = async (req, res, next) => {
                 .limit(limit)
                 .skip(skip)
                 .populate('MainPhoto Tag')
-                .sort('-startedAt')
+                .sort('-createdAt')
                 .execAsync(),
             totalCursor
                 .where('isTrashed').equals(false)
