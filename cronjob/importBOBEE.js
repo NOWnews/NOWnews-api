cc60933639ccf47226ce269194244259c16376ac
Leo Chang <leomax0725@gmail.com>
2018-03-05 14:47:13 +0800

    先把過期時間關掉

--- cronjob/importBOBEE.js
+++ cronjob/importBOBEE.js
@@ -70,10 +70,10 @@ module.exports = new cron.CronJob({
 
                 // 如果不是在設定的時間區間內的新聞，就不需要收錄 #######
                 let newsPubDate = moment.tz(new Date(item.pubDate), 'Asia/Taipei');
-                if( newsPubDate.isBefore(prevTime) ) {
-                    debug('不收錄原因: 新聞資料過期');
-                    continue;
-                }
+                // if( newsPubDate.isBefore(prevTime) ) {
+                //     debug('不收錄原因: 新聞資料過期');
+                //     continue;
+                // }
 
                 // 確認對方給的新聞 url 是否符合規範，不符合規範就不收錄
                 let regexString = /^(http|https):\/\/bobee.nownews.com\//;
